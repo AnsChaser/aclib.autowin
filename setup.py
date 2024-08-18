e312ef483239c35330451fbe60ab2e9e958b1ec1
AnsChaser <anschaser@163.com>
2024-08-18 18:58:35 +0800

    pkginfo: update version 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='aclib.autowin',
-    version='1.0.7',
+    version='1.1.0',
     author='AnsChaser',
     author_email='anschaser@163.com',
     description='auto window task',
